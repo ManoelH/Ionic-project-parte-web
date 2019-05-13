87c12cd9cd54e09d50d64029d430c1500bb6ea05
manoel <henriqe115@gmail.com>
2019-05-13 15:43:38 -0300

    tela de login e cadastro de artistas

---
+++ js/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+//var firebase = require("firebase");
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
